4c6fb44f4e3f88430d8814ad012a19196408670a
Derek "Turtle" Roe <vgturtle127@gmail.com>
2015-07-05 19:26:12 -0500

    See description
    Renamed all the files to be more Git friendly (also it looks nicer
    :smiley: )
    Fix a few comments and some links
    Fix some directory names
    Update Lua and zlib (WIP)

---
+++ psxjin/decodexa.h
@@ -0,0 +1,38 @@
+//============================================
+//=== Audio XA decoding
+//=== Kazzuya
+//============================================
+
+#ifndef DECODEXA_H
+#define DECODEXA_H
+
+#include "PsxCommon.h"
+#include "emufile.h"
+
+class EMUFILE;
+
+struct ADPCM_Decode_t
+{
+	s32	y0, y1;
+	void save(EMUFILE* fp);
+	void load(EMUFILE* fp);
+};
+
+struct xa_decode_t
+{
+	s32				freq;
+	s32				nbits;
+	s32				stereo;
+	s32				nsamples;
+	ADPCM_Decode_t	left, right;
+	s16			pcm[16384];
+
+	void save(EMUFILE* fp);
+	void load(EMUFILE* fp);
+};
+
+
+long xa_decode_sector( xa_decode_t *xdp,
+                       unsigned char *sectorp,
+                       int is_first_sector );
+#endif
